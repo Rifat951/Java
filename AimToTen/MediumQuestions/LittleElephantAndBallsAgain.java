db2fce6dede5e76ac076b855936ec450b2918005
Rifat Hossain <31892050+Rifat951@users.noreply.github.com>
2021-08-15 02:16:23 +0600

    Update LittleElephantAndBallsAgain.java
    New Version

--- AimToTen/MediumQuestions/LittleElephantAndBallsAgain.java
+++ AimToTen/MediumQuestions/LittleElephantAndBallsAgain.java
@@ -2,19 +2,29 @@ import java.util.Stack;
 
 public class LittleElephantAndBallsAgain {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         String str = "RGBRGB";
         int count = 0;
-        Stack<Character> stk = new Stack<>();
-        for(int i = 0 ; i<str.length();i++){
-            for(int j = i  ; j <= i; j ++){
-               System.out.println(str.charAt(j));
-                stk.push((str.charAt(j)));
+        for(int i = 0; i < str.length(); i++){
+            for (int j = 0 ; j <= i ; j++){
+                str = str.substring(i,str.length()-i);
+               // System.out.println(str);
+                if(str.length()/2>j){
+                    if(str.charAt(i) == str.charAt(i+1)){
+                        System.out.println("i"+str.charAt(i));
+                    }
+                    else {
+                        count+=2;
+                    }
+                }
+                else if(str.length()==2) {
+                    str = str.substring(i,str.length()-i);
+                    count ++;
+                }
+                else {
+                    break;
+                }
             }
-        }
-        for (int i = stk.size(); stk.size() > 1; i--){
-            stk.pop();
-            count++;
         }
         System.out.println(count);
     }
