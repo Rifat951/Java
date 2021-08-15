2318fc0b4511e04aa31fa42b141bb6c80b9dd22a
Rifat Hossain <31892050+Rifat951@users.noreply.github.com>
2021-08-15 21:43:16 +0600

    Create Xylophone.java

---
+++ AimToTen/MediumQuestions/Xylophone.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+
+public class Xylophone {
+
+    public static int countKeys(int[] keys){
+
+        HashMap <Integer,Character> details = new HashMap<Integer,Character>();
+        int k =1;
+        for(int i = 1; i<=50; i++){
+            details.put(i,(char)(64+k));
+            k++;
+            if(k ==8){
+                k =1;
+            }
+        }
+        StringBuilder strb = new StringBuilder();
+        for(int i = 0 ; i < keys.length; i++){
+            // System.out.println(details.get(arr[i]));
+            strb.append(details.get(keys[i]));
+        }
+        
+        
+        LinkedHashSet<Character> set = new LinkedHashSet<>();
+        for(int i=0;i<strb.length();i++){
+            set.add(strb.charAt(i));
+        }
+        int count =0;
+        for(Character ch : set) {
+        //    System.out.print(ch);
+            count++;
+        }
+        return  count;
+    }
+
+
+    public static void main(String[] args){
+
+        int[] arr = {1,8,3};
+        System.out.println(countKeys(arr));
+
+    }
+}
