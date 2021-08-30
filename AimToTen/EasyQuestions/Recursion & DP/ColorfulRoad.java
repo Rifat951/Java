c2bff5a39cc1d7fe4b1e7fddb0a2c397f00fe30c
Rifat Hossain <31892050+Rifat951@users.noreply.github.com>
2021-08-30 21:07:20 +0600

    Update ColorfulRoad.java

--- AimToTen/EasyQuestions/Recursion & DP/ColorfulRoad.java
+++ AimToTen/EasyQuestions/Recursion & DP/ColorfulRoad.java
@@ -1,53 +1,27 @@
-import java.util.Arrays;
-import java.util.HashMap;
+
 public class ColorfulRoad {
 
+    public static void main(String[] args) {
+        String road = "RGB";
+        int res = getMin(road);
+        System.out.println(res);
+    }
     public static int getMin(String road) {
-        int n = road.length();
-        char[] s = road.toCharArray();
-        HashMap<Character, Character> map = new HashMap<Character, Character>();
-        map.put('R', 'G');
-        map.put('G', 'B');
-        map.put('B', 'R');
-        int min = Integer.MAX_VALUE;
-        int left = (int) Math.round(Math.pow(2, n - 1));
-        int right = (int) Math.round(Math.pow(2, n));
-        for (int i = left; i < right; i++) {
-            String status = Integer.toBinaryString(i);
-            if (status.charAt(status.length() - 1) == '0') {
-                continue;
-            }
-            int cost = 0;
-            int prev = 0;
-            char prevC = 'R';
-            boolean valid = true;
-            for (int j = status.indexOf("1", 1); j < n && j > 0; j = status.indexOf("1", j + 1)) {
-                if (s[j] == map.get(prevC)) {
-                    cost += (j - prev) * (j - prev);
-                    prevC = s[j];
-                    prev = j;
-                } else {
-                    valid = false;
-                    break;
-                }
-            }
 
-            if (valid) {
-                min = Math.min(min, cost);
+        int[]n = new int[20];
+        for (int i = 0; i < n.length; i++) n[i] = Integer.MAX_VALUE;
+        n[0] = 0;
+        for (int i = 0; i < road.length(); i++) {
+            for (int j = i+1; j < road.length(); j++) {
+                if (road.charAt(i)=='R' && road.charAt(j)=='G')
+                    n[j] =(int) Math.min(n[j], n[i]+Math.pow((j-i), 2));
+                if (road.charAt(i)=='G' && road.charAt(j)=='B')
+                    n[j] =(int) Math.min(n[j], n[i]+Math.pow((j-i), 2));
+                if (road.charAt(i)=='B' && road.charAt(j)=='R')
+                    n[j] =(int) Math.min(n[j], n[i]+Math.pow((j-i), 2));
             }
         }
-        if (min == Integer.MAX_VALUE) {
-            return -1;
-        }
-        return min;
+        if (n[road.length()-1]==Integer.MAX_VALUE) return -1;
+        return n[road.length()-1];
     }
-
-
-
-    public static void main(String[] args){
-        System.out.println(getMin("RG"));
-
-
-    }
-
 }
